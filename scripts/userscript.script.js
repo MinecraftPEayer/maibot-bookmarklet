5617718da4766abbba2979997b63fe24d6d9ac58
MinecraftPEayer <87757022+MinecraftPEayer@users.noreply.github.com>
2026-01-29 18:30:56 +0800

    Create scripts/userscript.script.js

---
+++ scripts/userscript.script.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         maibot-bookmarklet
+// @version      0.1
+// @description  uwu
+// @author       Bookmarklet script loader for MinecraftPEayer/maibot
+// @match        https://maimaidx-eng.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const s = document.createElement('script');
+    const cacheBuster = Math.floor(Date.now()/60000);
+    s.src = 'https://minecraftpeayer.me/maibot-bookmarklet/main.bundle.js?t=' + cacheBuster;
+    document.body.append(s);
+})();
