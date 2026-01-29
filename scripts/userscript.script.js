dec6dbd89093d18b5cc3d4c822e89291fca49246
MinecraftPEayer <boingli227@outlook.com>
2026-01-29 12:32:25 +0000

    fix: Fix script metadata formatting and improve readability

--- scripts/userscript.script.js
+++ scripts/userscript.script.js
@@ -1,17 +1,19 @@
 // ==UserScript==
 // @name         maibot-bookmarklet
 // @version      0.1
-// @description  uwu
-// @author       Bookmarklet script loader for MinecraftPEayer/maibot
+// @description  Bookmarklet script loader for MinecraftPEayer/maibot
+// @author       MinecraftPEayer
 // @match        https://maimaidx-eng.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
     const s = document.createElement('script');
-    const cacheBuster = Math.floor(Date.now()/60000);
-    s.src = 'https://minecraftpeayer.me/maibot-bookmarklet/main.bundle.js?t=' + cacheBuster;
+    const cacheBuster = Math.floor(Date.now() / 60000);
+    s.src =
+        'https://minecraftpeayer.me/maibot-bookmarklet/main.bundle.js?t=' +
+        cacheBuster;
     document.body.append(s);
 })();
